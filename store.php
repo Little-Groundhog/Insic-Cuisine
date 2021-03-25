87bc8a9bc41194f09fc35467e54fb8ef6d7b2de6
Maxime Belcour <maximebelcour@gmail.com>
2021-03-25 18:18:05 +0100

    fix le bug des enfers sur la page store

--- store.php
+++ store.php
@@ -236,8 +236,6 @@
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/js/bs-init.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
     <script src="assets/js/Animated-Typing-With-Blinking.js"></script>
     <script src="assets/js/current-day.js"></script>
 </body>
